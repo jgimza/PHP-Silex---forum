304d44d232194a232813cc4cca5846afc0143ed4
jgimza <35743730+jgimza@users.noreply.github.com>
2018-08-02 06:29:31 +0200

    Add files via upload

---
+++ src/app.php
@@ -0,0 +1,57 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+use Silex\Provider\LocaleServiceProvider;
+use Silex\Provider\TranslationServiceProvider;
+use Silex\Provider\DoctrineServiceProvider;
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+$app->register(
+    new DoctrineServiceProvider(),
+    [
+        'db.options' => [
+            'driver'    => 'pdo_mysql',
+            'host'      => 'localhost',
+            'dbname'    => 'test',
+            'user'      => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'driverOptions' => [
+                1002 => 'SET NAMES utf8',
+            ],
+        ],
+    ]
+);
+
+$app->register(new LocaleServiceProvider());
+$app->register(
+    new TranslationServiceProvider(),
+    [
+        'locale' => 'pl',
+        'locale_fallbacks' => array('en'),
+    ]
+);
+$app->extend('translator', function ($translator, $app) {
+    $translator->addResource('xliff', __DIR__.'/../translations/messages.en.xlf', 'en', 'messages');
+    $translator->addResource('xliff', __DIR__.'/../translations/validators.en.xlf', 'en', 'validators');
+    $translator->addResource('xliff', __DIR__.'/../translations/messages.pl.xlf', 'pl', 'messages');
+    $translator->addResource('xliff', __DIR__.'/../translations/validators.pl.xlf', 'pl', 'validators');
+
+    return $translator;
+});
+
+return $app;
---
+++ src/controllers.php
@@ -0,0 +1,52 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Controller\FaqController;
+use Controller\CommunityController;
+use Controller\SectionController;
+use Controller\TopicController;
+use Repository\SectionRepository;
+use Repository\SubforumRepository;
+
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+$app->get('/', function () use ($app) {
+    $sectionRepository = new SectionRepository($app['db']);
+    $subforumRepository = new SubforumRepository($app['db']);
+
+    return $app['twig']->render(
+        'index.html.twig',
+        [
+            'subforum' => $subforumRepository->findAll($app),
+            'section' => $sectionRepository->findAll($app),
+            'topics' => $sectionRepository->findTopicData($app)
+        ]
+    );
+})
+    ->bind('homepage')
+;
+
+$app->mount('/faq', new FaqController());
+$app->mount('/community', new CommunityController());
+$app->mount('/', new SectionController());
+$app->mount('/{slug}/topic/', new TopicController());
+
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
