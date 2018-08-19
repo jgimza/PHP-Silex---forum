5ba3a4200f794065321725606b76125f70c7e449
jgimza <35743730+jgimza@users.noreply.github.com>
2018-08-19 18:22:38 +0200

    Add files via upload

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * Register form.
+ */
+
+namespace Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
+use Validator\Constraints as CustomAssert;
+
+/**
+ * Class LoginType.
+ */
+class RegisterType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'username',
+            TextType::class,
+            [
+                'label' => 'Login',
+                'required' => true,
+                'attr' => [
+                    'max_length' => 32,
+
+                ],
+                'constraints' => [
+                    new CustomAssert\UniqueLogin(
+                        [
+                            'repository' => $options['user_repository'],
+                        ]
+                    ),
+                    new Assert\NotBlank(),
+                    new Assert\Length(
+                        [
+                            'min' => 3,
+                            'max' => 32,
+                        ]
+                    ),
+                ],
+            ]
+        );
+        $builder->add(
+            'password',
+            PasswordType::class,
+            [
+                'label' => 'Hasło',
+                'required' => true,
+                'attr' => [
+                    'max_length' => 32,
+
+                ],
+                'constraints' => [
+                    new Assert\NotBlank(),
+                    new Assert\Length(
+                        [
+                            'min' => 3,
+                            'max' => 32,
+                        ]
+                    ),
+                ],
+            ]
+        );
+    }
+
+
+    /**
+     * Configure options
+     *
+     * @param \Symfony\Component\OptionsResolver\OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'user_repository' => null,
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'register_type';
+    }
+}
